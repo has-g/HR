6c92cb941d0cdd24cb453555901224756f5d4e88
has.golam@gmail.com <M0derate&7>
2020-03-15 19:59:04 +0000

    updated

--- qc_stringreverse.py
+++ qc_stringreverse.py
@@ -15,7 +15,7 @@ see ic_6_Fibonnaci.py
 3. Do you expect inplace or out of place manipulation
 4. assumption: delimiter = ' '
 '''
-def reverseString1(mystr):
+def reverseString(mystr):
     strlen = len(mystr)
 
     # check for zero string
@@ -46,31 +46,50 @@ def reverseString1(mystr):
 
     return tempstr
 
-def reverseString(mystr):
-    strlen = len(mystr)
+def swapStringInPlace(mylist, lidx, ridx):
+    '''
+    # 0 and single value
+    if len(myL) == 0: return myL
+    if len(myL) == 1: return myL
 
-    # check for zero string
-    if strlen == 0: return -1
+    # very important - this is in-place list manipulation
+    if ((lidx < 0) or (ridx > len(myL) - 1)): return
+    '''
 
-    # check for single char
-    if strlen == 1: return mystr
+    print('received l = ', lidx, ' r = ', ridx)
+    while lidx < ridx:
+        mylist[lidx], mylist[ridx] = mylist[ridx], mylist[lidx]
+        lidx += 1
+        ridx -= 1
 
-    tempword = ''
-    tempstr = ''
+def stringreverse(mylist):
+    # 0 and single value
+    if len(mylist) == 0: return
+    if len(mylist) == 1: return
 
-    for i in range(strlen - 1, -1, -1):
-        if ((mystr[i] == ' ') or (i == -1)):
-            print('tempword = ', tempword)
-            for j in range(len(tempword) - 1, -1, -1):
-                #print(tempword[j])
-                tempstr += tempword[j]
-            tempstr += ' '
-            tempword = ''
-        else:
-            tempword += mystr[i]
+    mylistlen = len(mylist) - 1
+    swapStringInPlace(mylist, 0, mylistlen)
+    print(mylist)
 
-    # handle for the final non-null value
-    print(tempword)
-    return tempstr
+    # Complexity
+    # O(n) time and O(1) space!
 
-print(reverseString('This is a length'))
+    lidx = 0
+
+    for i in range(len(mylist) + 1):
+        if (i == len(mylist) or (mylist[i] == ' ')):
+            swapStringInPlace(mylist, lidx, i - 1)
+            print(mylist)
+            lidx = i + 1
+
+#print(reverseString('This is a length'))
+mylist = ['T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 's', 't', 'r', 'i', 'n', 'g']
+stringreverse(mylist)
+print(mylist)
+'''
+swapStringInPlace(mylist, 6, 6)
+print(mylist)
+
+swapStringInPlace(mylist, 9, 10)
+print(mylist)
+'''
