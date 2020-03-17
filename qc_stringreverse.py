cd2d431cb0e531d1ff8ce1d3e7eb428bf583a6d0
has-g <62215297+has-g@users.noreply.github.com>
2020-03-17 20:50:28 +0000

    Update qc_stringreverse.py
    updated from lesson notes

--- qc_stringreverse.py
+++ qc_stringreverse.py
@@ -13,64 +13,102 @@ see ic_6_Fibonnaci.py
 1. what are we optimising for? Time or space?
 2. assumptions: non empty string or list
 3. Do you expect inplace or out of place manipulation
-4. assumption: delimiter = ' '
+4. assumption: delimiter = ' ' 
+5. assumption: no leading or trailing spaces?
 '''
-def reverseString1(mystr):
+
+#(A)
+#BRUTE FORCE APPROACH
+# Overall = O(n) runtime, space = O(1)
+
+
+def bf_reversewords(mystr):
+    if len(mystr) <= 1: return mystr
+    # e.g. "This is a girl" o/p => "girl a is This"
+
+    # edge case of space at beginning or end of string, retain the space for later
+
+    # split the list into constituent words ['This' 'is' 'a' 'girl']
+    mywords = mystr.split()
+
+    temp = ''
+    mywords = []
+
+    # O(n) runtime
+    # space = O(1)
+    for j in range(len(mystr)):
+        if mystr[j] != ' ':
+            temp += mystr[j]
+        elif (mystr[j] == ' '):
+            mywords.append(temp)
+            mywords.append(' ')
+            temp = ''
+        else:
+            print('whoa!')
+    mywords.append(temp)
+
+    print('mywords = ', mywords)
+
+    # create new string
+    mynewstr = ''
+
+    # O(n) runtime
+    # space = O(n)
+    # step back from end to beginning
+    for i in range(len(mywords) - 1, -1, -1):
+        # make up the new string sentence
+        mynewstr += mywords[i]
+        #mynewstr += ' '
+
+    return mynewstr
+
+print(bf_reversewords("This is a girl"))
+
+print('===============')
+
+# OPTION B
+# O(1) Space optimised, since string is manipulated in place
+
+def swapString(mystr, l, r):
+
+    while l < r:
+        mystr[l], mystr[r] = mystr[r], mystr[l]
+        l += 1
+        r -= 1
+
+myList = ['T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'b', 'o', 'y']
+#swapString(mystr, 0, len(mystr) - 1)
+print("mystr = ", myList)
+print('===============')
+
+
+def reverseList(mystr):
     strlen = len(mystr)
 
-    # check for zero string
-    if strlen == 0: return -1
+    if strlen <= 1: return mystr
 
-    # check for single char
-    if strlen == 1: return mystr
+    # swap the original string
+    swapString(mystr, 0, strlen - 1)
+    print(myList)
 
-    tempword = ''
-    tempstr = ''
-
-    for i in range(strlen - 1, -1, -1):
-        if mystr[i] != ' ':
-            tempword += mystr[i]
+    # now step through and reverse those that are within spaces or end of list
+    l = 0
+    r = 0
+    for i in range(len(mystr) + 1):
+        if (i == len(mystr)):
+            #print('final l = ', l, ' r = ', i - 1)
+            swapString(mystr, l, i - 1)
+            break
+        elif (mystr[i] == ' '):
+            #print('l = ', l, ' r = ', i - 1)
+            swapString(mystr, l, i - 1)
+            l = i + 1
         else:
-            print('tempword = ', tempword)
-            for j in range(len(tempword) - 1, -1, -1):
-                #print(tempword[j])
-                tempstr += tempword[j]
+            pass
 
-            tempstr += ' '
-            tempword = ''
 
-    # handle for the final non-null value
-    print(tempword)
-    for k in range(len(tempword) - 1, -1, -1):
-        tempstr += tempword[k]
 
-    return tempstr
+reverseList(myList)
+print(myList)
+print('..........')
 
-def reverseString(mystr):
-    strlen = len(mystr)
-
-    # check for zero string
-    if strlen == 0: return -1
-
-    # check for single char
-    if strlen == 1: return mystr
-
-    tempword = ''
-    tempstr = ''
-
-    for i in range(strlen - 1, -1, -1):
-        if ((mystr[i] == ' ') or (i == -1)):
-            print('tempword = ', tempword)
-            for j in range(len(tempword) - 1, -1, -1):
-                #print(tempword[j])
-                tempstr += tempword[j]
-            tempstr += ' '
-            tempword = ''
-        else:
-            tempword += mystr[i]
-
-    # handle for the final non-null value
-    print(tempword)
-    return tempstr
-
-print(reverseString('This is a length'))
